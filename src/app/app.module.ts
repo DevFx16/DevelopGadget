ead6de5ea0810b4270137b70ef0780edb6439d06
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-11-29 17:43:26 -0500

    Loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { InicioComponent } from './Components/inicio/inicio.component';
 
+//Rutas
+const Rutas: Routes = [
+  { path: 'Inicio', component: InicioComponent },
+  { path: '**', redirectTo: 'Inicio' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    RouterModule.forRoot(Rutas),
   ],
   providers: [],
   bootstrap: [AppComponent]
