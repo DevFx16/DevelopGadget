da8b74dd156294b5bb86902a1e9931c33d908b8d
Fernando Araujo <fernandoaraujo081@gmail.com>
2019-01-21 22:38:34 -0500

    Nuevos iconos

--- src/app/Components/inicio/inicio.component.ts
+++ src/app/Components/inicio/inicio.component.ts
@@ -11,17 +11,17 @@ export class InicioComponent implements OnInit {
   Loader: boolean = false;
   Ruta: string = '../../../assets/ES.json';
   Textos: any;
+  Iconos: object = ['https://image.flaticon.com/icons/svg/1383/1383260.svg', 'https://image.flaticon.com/icons/svg/25/25231.svg', 'https://image.flaticon.com/icons/svg/1383/1383262.svg', 'https://image.flaticon.com/icons/svg/1295/1295555.svg']
 
-  constructor() { }
+  constructor() { this.Cambio(this.Ruta); }
 
   ngOnInit() {
-    this.Cambio(this.Ruta);
     setTimeout(() => {
       this.Loader = true;
     }, 2000);
   }
 
-  Cambio(event){
+  Cambio(event: string) {
     this.Ruta = event;
     fetch(this.Ruta).then(json => json.json().then(json2 => this.Textos = json2));
   }
--- src/app/Components/nav-bar/nav-bar.component.ts
+++ src/app/Components/nav-bar/nav-bar.component.ts
@@ -9,14 +9,16 @@ export class NavBarComponent implements OnInit {
 
   @Output() Ruta = new EventEmitter();
   Idioma: string = 'ES';
+  Textos: any;
 
   constructor() { this.Ruta.emit('../../../assets/' + this.Idioma + '.json'); }
 
-  ngOnInit() { }
+  ngOnInit() { fetch('../../../assets/' + this.Idioma + '.json').then(json => json.json().then(json2 => this.Textos = json2)); }
 
   Cambio(Prefijo: string) {
     this.Idioma = Prefijo;
     this.Ruta.emit('../../../assets/' + this.Idioma + '.json');
+    fetch('../../../assets/' + this.Idioma + '.json').then(json => json.json().then(json2 => this.Textos = json2));
   }
 
 }
