d711e2cb04574407cbb69bd4166511e46d97e5dd
DevelopGadget <DevelopGadget@gmail.com>
2019-02-05 17:16:40 -0500

    Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { DatosService } from './Service/datos.service';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './Components/inicio/inicio.component';
 import { AppsComponent } from './Components/apps/apps.component';
+import { HttpClientModule } from '@angular/common/http';
 
 //Rutas
 const Rutas: Routes = [
@@ -22,9 +23,10 @@ const Rutas: Routes = [
   imports: [
     BrowserModule,
     RouterModule,
+    HttpClientModule,
     RouterModule.forRoot(Rutas),
   ],
-  providers: [],
+  providers: [DatosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
